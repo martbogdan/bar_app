df9fae2a6f1fb64583aad7869709c51fb62f5468
martbogdan <mbogdan0123@gmail.com>
2020-05-02 18:39:00 +0300

    drink Class

---
+++ app/src/main/java/com/example/barcoctail/Drink.java
@@ -0,0 +1,132 @@
+package com.example.barcoctail;
+
+import java.util.Date;
+
+public class Drink {
+    private Long idDrink;
+    private String strDrink;
+    private String strDrinkAlternate;
+    private String strTags;
+    private String strCategory;
+    private String strIBA;
+    private String strAlcoholic;
+    private String strGlass;
+    private String strInstructions;
+    private String strDrinkThumb;
+    private String[] ingredients = new String[15];
+    private String[] measures = new String[15];
+    private String strCreativeCommonsConfirmed;
+    private Date dateModified;
+
+    public Long getIdDrink() {
+        return idDrink;
+    }
+
+    public void setIdDrink(Long idDrink) {
+        this.idDrink = idDrink;
+    }
+
+    public String getStrDrink() {
+        return strDrink;
+    }
+
+    public void setStrDrink(String strDrink) {
+        this.strDrink = strDrink;
+    }
+
+    public String getStrDrinkAlternate() {
+        return strDrinkAlternate;
+    }
+
+    public void setStrDrinkAlternate(String strDrinkAlternate) {
+        this.strDrinkAlternate = strDrinkAlternate;
+    }
+
+    public String getStrTags() {
+        return strTags;
+    }
+
+    public void setStrTags(String strTags) {
+        this.strTags = strTags;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public void setStrCategory(String strCategory) {
+        this.strCategory = strCategory;
+    }
+
+    public String getStrIBA() {
+        return strIBA;
+    }
+
+    public void setStrIBA(String strIBA) {
+        this.strIBA = strIBA;
+    }
+
+    public String getStrAlcoholic() {
+        return strAlcoholic;
+    }
+
+    public void setStrAlcoholic(String strAlcoholic) {
+        this.strAlcoholic = strAlcoholic;
+    }
+
+    public String getStrGlass() {
+        return strGlass;
+    }
+
+    public void setStrGlass(String strGlass) {
+        this.strGlass = strGlass;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public void setStrInstructions(String strInstructions) {
+        this.strInstructions = strInstructions;
+    }
+
+    public String getStrDrinkThumb() {
+        return strDrinkThumb;
+    }
+
+    public void setStrDrinkThumb(String strDrinkThumb) {
+        this.strDrinkThumb = strDrinkThumb;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String[] getMeasures() {
+        return measures;
+    }
+
+    public void setMeasures(String[] measures) {
+        this.measures = measures;
+    }
+
+    public String getStrCreativeCommonsConfirmed() {
+        return strCreativeCommonsConfirmed;
+    }
+
+    public void setStrCreativeCommonsConfirmed(String strCreativeCommonsConfirmed) {
+        this.strCreativeCommonsConfirmed = strCreativeCommonsConfirmed;
+    }
+
+    public Date getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Date dateModified) {
+        this.dateModified = dateModified;
+    }
+}
