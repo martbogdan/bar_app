6c07f783e0a205161b176fedb1bb3517cdc7387d
martbogdan <mbogdan0123@gmail.com>
2020-05-02 17:22:23 +0300

    implemented get and pars from JSON

--- app/src/main/java/com/example/barcoctail/SearchScreen.java
+++ app/src/main/java/com/example/barcoctail/SearchScreen.java
@@ -2,6 +2,7 @@ package com.example.barcoctail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 
 import com.example.barcoctail.utils.NetUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URL;
 
@@ -17,6 +22,40 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    class DrinkQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = NetUtils.getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            String strDrink = null;
+            String strCategory = null;
+            String strAlcoholic = null;
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray drinks = jsonResponse.getJSONArray("drinks");
+                JSONObject drink = drinks.getJSONObject(1);
+
+                strDrink = drink.getString("strDrink");
+                strCategory = drink.getString("strCategory");
+                strAlcoholic = drink.getString("strAlcoholic");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String resultString = "Cocktail: " + strDrink + "\n" +
+                    "Category: " + strCategory + "\n" + "Alcoholic: " + strAlcoholic;
+            result.setText(resultString);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +71,7 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         URL generatedURL = NetUtils.generateURL(searchField.getText().toString());
-        String response = null;
-        try {
-            response = NetUtils.getResponseFromURL(generatedURL);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        result.setText(response);
+        new DrinkQueryTask().execute(generatedURL);
+
     }
 }
