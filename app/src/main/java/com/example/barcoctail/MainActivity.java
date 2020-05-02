a401499070b2b823f041c77112a40ecfe090572d
martbogdan <mbogdan0123@gmail.com>
2020-05-02 12:31:21 +0300

    added search activity

--- app/src/main/java/com/example/barcoctail/MainActivity.java
+++ app/src/main/java/com/example/barcoctail/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.barcoctail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSearch:
+                Intent intent = new Intent(this, SearchScreen.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
     }
 }
