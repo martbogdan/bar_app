0b0536657daff30348d0c46d94199e930e67dd85
martbogdan <mbogdan0123@gmail.com>
2020-05-03 19:22:22 +0300

    pars json and save it as drink object

--- app/src/main/java/com/example/barcoctail/Drink.java
+++ app/src/main/java/com/example/barcoctail/Drink.java
@@ -1,5 +1,7 @@
 package com.example.barcoctail;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Drink {
@@ -13,8 +15,10 @@ public class Drink {
     private String strGlass;
     private String strInstructions;
     private String strDrinkThumb;
-    private String[] ingredients = new String[15];
-    private String[] measures = new String[15];
+    private String ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6;
+    private String ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, ingredient12;
+    private String ingredient13, ingredient14, ingredient15;
+    private String measure1, measure2, measure3, measure4, measure5, measure6, measure7, measure8, measure9, measure10, measure11, measure12, measure13, measure14, measure15;
     private String strCreativeCommonsConfirmed;
     private Date dateModified;
 
@@ -22,8 +26,8 @@ public class Drink {
         return idDrink;
     }
 
-    public void setIdDrink(Long idDrink) {
-        this.idDrink = idDrink;
+    public void setIdDrink(String idDrink) {
+        this.idDrink = Long.parseLong(idDrink);
     }
 
     public String getStrDrink() {
@@ -98,20 +102,244 @@ public class Drink {
         this.strDrinkThumb = strDrinkThumb;
     }
 
-    public String[] getIngredients() {
-        return ingredients;
+    public String getIngredient1() {
+        return ingredient1;
     }
 
-    public void setIngredients(String[] ingredients) {
-        this.ingredients = ingredients;
+    public void setIngredient1(String ingredient1) {
+        this.ingredient1 = ingredient1;
     }
 
-    public String[] getMeasures() {
-        return measures;
+    public String getIngredient2() {
+        return ingredient2;
     }
 
-    public void setMeasures(String[] measures) {
-        this.measures = measures;
+    public void setIngredient2(String ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public String getIngredient3() {
+        return ingredient3;
+    }
+
+    public void setIngredient3(String ingredient3) {
+        this.ingredient3 = ingredient3;
+    }
+
+    public String getIngredient4() {
+        return ingredient4;
+    }
+
+    public void setIngredient4(String ingredient4) {
+        this.ingredient4 = ingredient4;
+    }
+
+    public String getIngredient5() {
+        return ingredient5;
+    }
+
+    public void setIngredient5(String ingredient5) {
+        this.ingredient5 = ingredient5;
+    }
+
+    public String getIngredient6() {
+        return ingredient6;
+    }
+
+    public void setIngredient6(String ingredient6) {
+        this.ingredient6 = ingredient6;
+    }
+
+    public String getIngredient7() {
+        return ingredient7;
+    }
+
+    public void setIngredient7(String ingredient7) {
+        this.ingredient7 = ingredient7;
+    }
+
+    public String getIngredient8() {
+        return ingredient8;
+    }
+
+    public void setIngredient8(String ingredient8) {
+        this.ingredient8 = ingredient8;
+    }
+
+    public String getIngredient9() {
+        return ingredient9;
+    }
+
+    public void setIngredient9(String ingredient9) {
+        this.ingredient9 = ingredient9;
+    }
+
+    public String getIngredient10() {
+        return ingredient10;
+    }
+
+    public void setIngredient10(String ingredient10) {
+        this.ingredient10 = ingredient10;
+    }
+
+    public String getIngredient11() {
+        return ingredient11;
+    }
+
+    public void setIngredient11(String ingredient11) {
+        this.ingredient11 = ingredient11;
+    }
+
+    public String getIngredient12() {
+        return ingredient12;
+    }
+
+    public void setIngredient12(String ingredient12) {
+        this.ingredient12 = ingredient12;
+    }
+
+    public String getIngredient13() {
+        return ingredient13;
+    }
+
+    public void setIngredient13(String ingredient13) {
+        this.ingredient13 = ingredient13;
+    }
+
+    public String getIngredient14() {
+        return ingredient14;
+    }
+
+    public void setIngredient14(String ingredient14) {
+        this.ingredient14 = ingredient14;
+    }
+
+    public String getIngredient15() {
+        return ingredient15;
+    }
+
+    public void setIngredient15(String ingredient15) {
+        this.ingredient15 = ingredient15;
+    }
+
+    public String getMeasure1() {
+        return measure1;
+    }
+
+    public void setMeasure1(String measure1) {
+        this.measure1 = measure1;
+    }
+
+    public String getMeasure2() {
+        return measure2;
+    }
+
+    public void setMeasure2(String measure2) {
+        this.measure2 = measure2;
+    }
+
+    public String getMeasure3() {
+        return measure3;
+    }
+
+    public void setMeasure3(String measure3) {
+        this.measure3 = measure3;
+    }
+
+    public String getMeasure4() {
+        return measure4;
+    }
+
+    public void setMeasure4(String measure4) {
+        this.measure4 = measure4;
+    }
+
+    public String getMeasure5() {
+        return measure5;
+    }
+
+    public void setMeasure5(String measure5) {
+        this.measure5 = measure5;
+    }
+
+    public String getMeasure6() {
+        return measure6;
+    }
+
+    public void setMeasure6(String measure6) {
+        this.measure6 = measure6;
+    }
+
+    public String getMeasure7() {
+        return measure7;
+    }
+
+    public void setMeasure7(String measure7) {
+        this.measure7 = measure7;
+    }
+
+    public String getMeasure8() {
+        return measure8;
+    }
+
+    public void setMeasure8(String measure8) {
+        this.measure8 = measure8;
+    }
+
+    public String getMeasure9() {
+        return measure9;
+    }
+
+    public void setMeasure9(String measure9) {
+        this.measure9 = measure9;
+    }
+
+    public String getMeasure10() {
+        return measure10;
+    }
+
+    public void setMeasure10(String measure10) {
+        this.measure10 = measure10;
+    }
+
+    public String getMeasure11() {
+        return measure11;
+    }
+
+    public void setMeasure11(String measure11) {
+        this.measure11 = measure11;
+    }
+
+    public String getMeasure12() {
+        return measure12;
+    }
+
+    public void setMeasure12(String measure12) {
+        this.measure12 = measure12;
+    }
+
+    public String getMeasure13() {
+        return measure13;
+    }
+
+    public void setMeasure13(String measure13) {
+        this.measure13 = measure13;
+    }
+
+    public String getMeasure14() {
+        return measure14;
+    }
+
+    public void setMeasure14(String measure14) {
+        this.measure14 = measure14;
+    }
+
+    public String getMeasure15() {
+        return measure15;
+    }
+
+    public void setMeasure15(String measure15) {
+        this.measure15 = measure15;
     }
 
     public String getStrCreativeCommonsConfirmed() {
@@ -126,7 +354,7 @@ public class Drink {
         return dateModified;
     }
 
-    public void setDateModified(Date dateModified) {
-        this.dateModified = dateModified;
+    public void setDateModified(String dateModified) throws ParseException {
+        this.dateModified = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateModified);
     }
 }
--- app/src/main/java/com/example/barcoctail/SearchScreen.java
+++ app/src/main/java/com/example/barcoctail/SearchScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.ParseException;
 
 public class SearchScreen extends AppCompatActivity implements View.OnClickListener {
     private EditText searchField;
@@ -37,32 +39,28 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
 
         @Override
         protected void onPostExecute(String response) {
-            String[] strDrink = null;
-            String[] strCategory = null;
-            String[] strAlcoholic = null;
+            Drink[] drinksFound = null;
             try {
                 JSONObject jsonResponse = new JSONObject(response);
                 JSONArray drinks = jsonResponse.getJSONArray("drinks");
                 JSONObject drink;
-                strDrink = new String[drinks.length()];
-                strCategory = new String[drinks.length()];
-                strAlcoholic = new String[drinks.length()];
+                drinksFound = new Drink[drinks.length()];
                 for (int i = 0; i < drinks.length(); i++) {
-                    drink = drinks.getJSONObject(i);
-                    strDrink[i] = drink.getString("strDrink");
-                    strCategory[i] = drink.getString("strCategory");
-                    strAlcoholic[i] = drink.getString("strAlcoholic");
+                    drinksFound[i] = getDrink(drinks.getJSONObject(i));
                 }
-            } catch (JSONException e) {
+            } catch (JSONException | ParseException e) {
                 e.printStackTrace();
             }
             StringBuilder resultString = new StringBuilder("");
-            for (int i = 0; i < strDrink.length; i++) {
-                String tmp = "Cocktail: " + strDrink[i] + "\n" +
-                        "Category: " + strCategory[i] + "\n" + "Alcoholic: " + strAlcoholic[i];
-                resultString.append(tmp).append("\n").append("=========").append("\n");
+            if (drinksFound != null && drinksFound.length > 0) {
+                for (int i = 0; i < drinksFound.length; i++) {
+                    String tmp = "Cocktail: " + drinksFound[i].getStrDrink() + "\n" +
+                            "Category: " + drinksFound[i].getStrCategory() + "\n" + "Alcoholic: " + drinksFound[i].getStrAlcoholic();
+                    resultString.append(tmp).append("\n").append("=========").append("\n");
+                }
+            } else {
+                resultString.append("No cocktails found");
             }
-
             result.setText(resultString.toString());
         }
     }
@@ -83,5 +81,52 @@ public class SearchScreen extends AppCompatActivity implements View.OnClickListe
         URL generatedURL = NetUtils.generateURL(searchField.getText().toString());
         new DrinkQueryTask().execute(generatedURL);
 
+    }
+
+    private static Drink getDrink(JSONObject jsonObject) throws JSONException, ParseException {
+        Drink drink = new Drink();
+        drink.setIdDrink(jsonObject.getString("idDrink"));
+        drink.setStrDrink(jsonObject.getString("strDrink"));
+        drink.setStrDrinkAlternate(jsonObject.getString("strDrinkAlternate"));
+        drink.setStrTags(jsonObject.getString("strTags"));
+        drink.setStrCategory(jsonObject.getString("strCategory"));
+        drink.setStrIBA(jsonObject.getString("strIBA"));
+        drink.setStrAlcoholic(jsonObject.getString("strAlcoholic"));
+        drink.setStrGlass(jsonObject.getString("strGlass"));
+        drink.setStrInstructions(jsonObject.getString("strInstructions"));
+        drink.setStrDrinkThumb(jsonObject.getString("strDrinkThumb"));
+        drink.setIngredient1(jsonObject.getString("strIngredient1"));
+        drink.setIngredient2(jsonObject.getString("strIngredient2"));
+        drink.setIngredient3(jsonObject.getString("strIngredient3"));
+        drink.setIngredient4(jsonObject.getString("strIngredient4"));
+        drink.setIngredient5(jsonObject.getString("strIngredient5"));
+        drink.setIngredient6(jsonObject.getString("strIngredient6"));
+        drink.setIngredient7(jsonObject.getString("strIngredient7"));
+        drink.setIngredient8(jsonObject.getString("strIngredient8"));
+        drink.setIngredient9(jsonObject.getString("strIngredient9"));
+        drink.setIngredient10(jsonObject.getString("strIngredient10"));
+        drink.setIngredient11(jsonObject.getString("strIngredient11"));
+        drink.setIngredient12(jsonObject.getString("strIngredient12"));
+        drink.setIngredient13(jsonObject.getString("strIngredient13"));
+        drink.setIngredient14(jsonObject.getString("strIngredient14"));
+        drink.setIngredient15(jsonObject.getString("strIngredient15"));
+        drink.setMeasure1(jsonObject.getString("strMeasure1"));
+        drink.setMeasure2(jsonObject.getString("strMeasure2"));
+        drink.setMeasure3(jsonObject.getString("strMeasure3"));
+        drink.setMeasure4(jsonObject.getString("strMeasure4"));
+        drink.setMeasure5(jsonObject.getString("strMeasure5"));
+        drink.setMeasure6(jsonObject.getString("strMeasure6"));
+        drink.setMeasure7(jsonObject.getString("strMeasure7"));
+        drink.setMeasure8(jsonObject.getString("strMeasure8"));
+        drink.setMeasure9(jsonObject.getString("strMeasure9"));
+        drink.setMeasure10(jsonObject.getString("strMeasure10"));
+        drink.setMeasure11(jsonObject.getString("strMeasure11"));
+        drink.setMeasure12(jsonObject.getString("strMeasure12"));
+        drink.setMeasure13(jsonObject.getString("strMeasure13"));
+        drink.setMeasure14(jsonObject.getString("strMeasure14"));
+        drink.setMeasure15(jsonObject.getString("strMeasure15"));
+        drink.setStrCreativeCommonsConfirmed(jsonObject.getString("strCreativeCommonsConfirmed"));
+        drink.setDateModified(jsonObject.getString("dateModified"));
+        return drink;
     }
 }
